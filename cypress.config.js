7f7d28af442d8e264b8175c9a5cdc5105073a8b0
thaimeo03 <thaitran15072003@gmail.com>
2025-05-09 00:18:09 +0700

    Test: user's detail problem

--- cypress.config.js
+++ cypress.config.js
@@ -6,8 +6,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    viewportWidth: 1280,
-    viewportHeight: 720,
-    watchForFileChanges: false,
+    viewportWidth: 1280, // set the viewport size
+    viewportHeight: 720, // set the viewport size
+    watchForFileChanges: false, // disable file watching
+    scrollBehavior: 'nearest', // scroll to the nearest element
   },
 })
