ae34ff452f9a3020cb5995dc990c8185a324ee01
Truc cute <buithanhtruc0608@gmail.com>
2025-05-14 23:15:27 +0700

    update lesson

---
+++ cypress/e2e/admin/lessons/update-lesson.cy.js
@@ -0,0 +1,138 @@
+describe('(Admin) update lesson',()=>{
+    const LESSON_ID = '7f4cc4cc-e039-482a-8f9f-50cd35fabda2'
+    beforeEach(()=>{
+        cy.loginAdmin()
+        cy.visit(`/admin/lecture/${LESSON_ID}/edit`)
+    })
+    describe('Check UI',() => {
+    // it('UDBG02 Kiểm tra tiêu đề [Tiêu đề bài giảng] texbox ',() => {
+    //     cy.get('label').contains('Tiêu đề bài giảng').should('be.visible')
+    // })
+    // it('UDBG03 Kiểm tra placeholder [Tiêu đề bài giảng] texbox  ',() => {
+    //     cy.get('bidv-input[formcontrolname ="title"] input').clear()
+    //     cy.get('bidv-input[formcontrolname ="title"]').find('label').should('contain.text','Nhập tiêu đề')
+    // })
+    // it('UDBG04  Kiểm tra tiêu đề [Số thứ tự] texbox  ',() => {
+    //     cy.get('label').contains('Số thứ tự').should('be.visible')
+    // })
+    // it('UDBG05 Kiểm tra placeholder [Số thứ tự] texbox ',() => {
+    //     cy.get('bidv-input[formcontrolname = "no"] input[type = "number"]').clear()
+    //     cy.get('bidv-input[formcontrolname = "no"]').find('label').should('contain.text','Nhập số thứ tự')
+    // })
+    // it('UDBG06 Kiểm tra tiêu đề [URL Video] textbox ',() => {
+    //     cy.get('label').contains('URL Video').should('be.visible')
+    // })
+    // it('UDBG07 Kiểm tra placeholder [URL Video] textbox ',() => {
+    //     cy.get('bidv-input[formcontrolname ="videoUrl"] input').clear()
+    //     cy.get('bidv-input[formcontrolname ="videoUrl"]').should('contain.text','Nhập URL video Youtube')
+    // })
+    // it('UDBG08 Kiểm tra tiêu đề [Nội dung] textbox ',() => {
+    //     cy.get('app-editor').should('contain.text', 'Nội dung')
+    // })
+    // it('UDBG09 tra placeholder [Nội dung] textbox ',() => {
+    //     cy.get('app-editor').should('contain.text', 'Nội dung')
+    // })
+    })
+    describe('Check validation',() => {
+    //     it('UDBG010: Kiểm tra khi bỏ trống [Tiêu đề bài giảng] textbox',() => {
+    //         cy.get('bidv-input[formcontrolname="title"] input').clear()
+    //         cy.get('button').contains('Cập nhật').click()
+    //         cy.get('bidv-error[formcontrolname="title"]').should('contain.text','Trường này không được để trống',
+    //   )
+    //     })
+    //     it('UDBG11: Kiểm tra khi nhập khoảng trắng vào [Tiêu đề bài giảng] textbox',() => {
+    //         cy.get('bidv-input[formcontrolname="title"] input').clear().type(' ')
+    //         cy.get('button').contains('Cập nhật').click()
+    //         cy.get('bidv-error[formcontrolname="title"]').should('contain.text','Trường này không được để trống')
+    //     })
+        // it('UDBG12:Kiểm tra khi nhập vào [Tiêu đề bài giảng] textbox chuỗi có độ dài bằng 1',() => {
+        //     cy.get('bidv-input[formcontrolname="title"] input').clear()
+        //     cy.get('bidv-input[formcontrolname="title"]').type('a')
+        //     cy.get('button').contains('Cập nhật').click()
+        //     cy.get('bidv-error[formcontrolname="title"]').should('be.empty')
+        // })
+        // it('UDBG13: Kiểm tra khi nhập vào [Tiêu đề bài giảng] textbox chuỗi có độ dài bằng 300',() => {
+        //     const inputTitle = 'a'.repeat(300)
+        //     cy.get('bidv-input[formcontrolname="title"] input').clear().type(inputTitle)
+        //     cy.get('button').contains('Cập nhật').click()
+        //     cy.get('bidv-error[formcontrolname="title"]').should('be.empty')
+        // })
+        // it('UDBG14: Kiểm tra khi nhập vào [Tiêu đề bài giảng] textbox chuỗi có độ dài bằng 301',() => {
+        //     const chars = 'a'.repeat(301)
+        //     cy.get('bidv-input[formcontrolname="title"] input').clear().type(chars)
+        //     cy.get('button').contains('Cập nhật').click()
+        //     cy.get('bidv-error[formcontrolname="title"]').should('contain.text','title must be shorter than or equal to 300 characters')
+        // })
+        // it('UDBG15: Kiểm tra khi nhâpj số vào [Tiêu đề bài giảng] textbox',() => {
+        //     cy.get('bidv-input[formcontrolname="title"] input').clear().type('123456789')
+        //     cy.get('button').contains('Cập nhật').click()
+        //     cy.get('bidv-error[formcontrolname="title"]').should('be.empty')
+        // })
+        // it('UDBG16: Kiểm tra khi nhập kí tự đặc biệt vào [Tiêu đề bài giảng] textbox ',() => {
+        //     cy.get('bidv-input[formcontrolname="title"] input').clear().type('!@#$%^&*')
+        //     cy.get('button').contains('Cập nhật').click()
+        //     cy.get('bidv-error[formcontrolname="title"]').should('be.empty')
+        // })
+        // it('UDBG17: Kiểm tra khi bỏ trống [Số thứ tự] textbox',() => {
+        //     cy.get('bidv-input[formcontrolname="no"] input[type="number"]').clear()
+        //     cy.get('button').contains('Cập nhật').click()
+        //     cy.get('bidv-error[formcontrolname="no"]').should('contain.text','Trường này không được để trống')
+        // })
+        // it('UDBG19: Kiểm tra khi nhập số vào [Số thứ tự] textbox',() => {
+        //     cy.get('bidv-input[formcontrolname="no"] input[type="number"]').clear().type(123)
+        //     cy.get('button').contains('Cập nhật').click()
+        //     cy.get('bidv-error[formcontrolname="no"]').should('be.empty')
+        // })
+        // it('UDBG20: Kiểm tra khi nhập kí tự chữ vào [Số thứ tự] textbox',() => {
+        //      cy.get('bidv-input[formcontrolname="no"]').type('abc').should('have.value', '');   
+        // })
+        // it('UDBG21: Kiểm tra khi bỏ trống [URL video] textbox',() => {
+        //     cy.get('bidv-input[formcontrolname="videoUrl"] input').clear()
+        //     cy.get('button').contains('Cập nhật').click()
+        //     cy.get('bidv-error[formcontrolname="videoUrl"]').should('contain.text','Trường này không được để trống')
+        // })
+        // it('UDBG22: Kiểm tra khi nhập khoảng trắng vào [URL video] textbox',() => {
+        //     cy.get('bidv-input[formcontrolname="videoUrl"] input').clear().type(' ')
+        //     cy.get('button').contains('Cập nhật').click()
+        //     cy.get('bidv-error[formcontrolname="videoUrl"]').should('contain.text','Giá trị không hợp lệ')
+        // })
+        // it('UDBG23: Kiểm tra khi nhập vào [URL video] textbox đúng định dạng url (youtube)',() => {
+        //     cy.get('bidv-input[formcontrolname="videoUrl"] input').clear().type('https://youtu.be/WS05AU6YYm4?list=PL33lvabfss1xagFyyQPRcppjFKMQ7lvJM')
+        //     cy.get('button').contains('Cập nhật').click()
+        //     cy.get('bidv-error[formcontrolname="videoUrl"]').should('be.empty')
+        // })
+        // it('UDBG24: Kiểm tra khi nhập vào [URL video] textbox đúng định dạng url (nhưng không phải yourtube)',() => {
+        //     cy.get('bidv-input[formcontrolname="videoUrl"] input').clear().type('https://hehe/WS05AU6YYm4?list=PL33lvabfss1xagFyyQPRcppjFKMQ7lvJM')
+        //     cy.get('button').contains('Cập nhật').click()
+        //     cy.get('bidv-error[formcontrolname="videoUrl"]').should('contain.text','Giá trị không hợp lệ')
+        // })
+        
+        // it('UDBG25: Kiểm tra khi nhập vào [URL video] textbox sai định dạng url  ',() => {
+        //     cy.get('bidv-input[formcontrolname="videoUrl"] input').clear().type('https://hehe/WS05AU6YYm4?list=PL33lvabfss1xagFyyQPRcppjFKMQ7lvJM')
+        //     cy.get('button').contains('Cập nhật').click()
+        //     cy.get('bidv-error[formcontrolname="videoUrl"]').should('contain.text','Giá trị không hợp lệ')
+        // })
+        // it('UDBG26: Kiểm tra khi bỏ trống [Nội dung] textbox',() => {
+        //     cy.get('bidv-input[formcontrolname="videoUrl"] input').clear().type('https://hehe/WS05AU6YYm4?list=PL33lvabfss1xagFyyQPRcppjFKMQ7lvJM')
+        //     cy.get('button').contains('Cập nhật').click()
+        //     cy.get('bidv-error[formcontrolname="videoUrl"]').should('contain.text','Giá trị không hợp lệ')
+        // })
+        // it('UDBG27: Kiểm tra khi nhập khoảng trắng vào [Nội dung] textbox',() => {
+        //     cy.get('bidv-input[formcontrolname="videoUrl"] input').clear().type('https://hehe/WS05AU6YYm4?list=PL33lvabfss1xagFyyQPRcppjFKMQ7lvJM')
+        //     cy.get('button').contains('Cập nhật').click()
+        //     cy.get('bidv-error[formcontrolname="videoUrl"]').should('contain.text','Giá trị không hợp lệ')
+        // })
+        // it('UDBG28: Kiểm tra khi nhập số vào [Nội dung] textbox',() => {
+        //     cy.get('bidv-input[formcontrolname="videoUrl"] input').clear().type('https://hehe/WS05AU6YYm4?list=PL33lvabfss1xagFyyQPRcppjFKMQ7lvJM')
+        //     cy.get('button').contains('Cập nhật').click()
+        //     cy.get('bidv-error[formcontrolname="videoUrl"]').should('contain.text','Giá trị không hợp lệ')
+        // })
+        // it('UDBG29: Kiểm tra khi nhập kí tự đặc biệt vào [Nội dung] textbox',() => {
+        //     cy.get('bidv-input[formcontrolname="videoUrl"] input').clear().type('https://hehe/WS05AU6YYm4?list=PL33lvabfss1xagFyyQPRcppjFKMQ7lvJM')
+        //     cy.get('button').contains('Cập nhật').click()
+        //     cy.get('bidv-error[formcontrolname="videoUrl"]').should('contain.text','Giá trị không hợp lệ')
+        // })
+
+    })
+
+})
